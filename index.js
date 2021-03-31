7426d62057891dabb415d7fedb5322bef3481e99
olaifa olawale <olaifaolawaleh@gmail.com>
2021-03-31 13:50:14 +0100

    little modifications to code indentations

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const baseUrl = 'https://official-joke-api.appspot.com/jokes/random';
 const content = document.querySelector('#joke');
-const button = document.getElementById('btn');
+const button = document.querySelector('#btn');
 
 
 //fetching API...
-    const getData = async () => {
-        const response = await fetch(`${baseUrl}`);
-        const data = await response.json();
-        return data;
-    }
-console.log(getData());
+const getData = async () => {
+    const response = await fetch(`${baseUrl}`);
+    const data = await response.json();
+    return data;
+}
+
 
 button.addEventListener('click', async () => {
     const getJoke = await getData();
 
-    content.innerHTML = `
-        <h2>${getJoke.setup}</h2>
-        <h3 id='sub'>${getJoke.punchline}</h3>
+//inputing the API content into the HTML DOM...
+content.innerHTML = `
+    <h2>${getJoke.setup}</h2>
+    <h3 id='sub'>${getJoke.punchline}</h3>
     `
 });
